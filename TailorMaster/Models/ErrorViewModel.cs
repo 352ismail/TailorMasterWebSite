a5dd1656c08a45f8bb7603a2efc21834c76e896b
ismail khan <352ismailkhan@gmail.com>
2023-07-19 16:16:16 +0500

    Upload-Project

---
+++ TailorMaster/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TailorMaster.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
