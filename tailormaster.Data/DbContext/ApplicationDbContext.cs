2e9b25e93c4b906eec8825307e2fcf213d9539c5
ismail khan <352ismailkhan@gmail.com>
2023-07-26 18:31:00 +0500

    add-login-table

--- tailormaster.Data/DbContext/ApplicationDbContext.cs
+++ tailormaster.Data/DbContext/ApplicationDbContext.cs
@@ -7,14 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using tailormaster.Data.Auth;
+using tailormaster.Domain.Models;
 
 namespace tailormaster.Data.DbContext
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<LoginTable> loginTable { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-
+            
         }
         protected override void OnModelCreating(ModelBuilder builder)
         {
---
+++ tailormaster.Data/Models/LoginTable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tailormaster.Domain.Models
+{
+    public class LoginTable
+    {
+        [Key]
+        public long LoginID { get; set; }
+        public string? UserID { get; set; }
+        public string? Email { get; set; }
+        public string Code { get; set; }
+        public bool IsVerified { get; set; } = false;
+        public DateTime TokenExpiry { get; set; } = DateTime.UtcNow.AddDays(7);SSSSSSS
+    }
+}
